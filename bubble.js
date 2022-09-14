e7805b2cd37834083ee94c4289e586838ded677e
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-14 22:29:33 +0200

    button + reload

--- bubble.js
+++ bubble.js
@@ -54,25 +54,35 @@ btn.addEventListener("click", () => {
   let sortNum = document.querySelectorAll("#tri > td");
   let i = 0;
 
-  function numberAppear() {
-    let active = false;
-    if (i < 10) {
-      setTimeout(() => {
-        num.forEach((e) => {
-          if (
-            e.textContent == sortNum[i].textContent &&
-            active === false &&
-            e.style.visibility !== "hidden"
-          ) {
-            e.style.visibility = "hidden";
-            active = true;
-          }
-        });
-        sortNum[i].style.visibility = "visible";
-        i++;
-        numberAppear();
-      }, 1000);
+  if (btn.value == "Reset") {
+    window.location.reload();
+  } else {
+    btn.style.visibility = "hidden";
+    function numberAppear() {
+      let active = false;
+      if (i < 10) {
+        setTimeout(() => {
+          num.forEach((e) => {
+            if (
+              e.textContent == sortNum[i].textContent &&
+              active === false &&
+              e.style.visibility !== "hidden"
+            ) {
+              e.style.visibility = "hidden";
+              active = true;
+            }
+          });
+          sortNum[i].style.visibility = "visible";
+          i++;
+          numberAppear();
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          btn.style.visibility = "visible";
+          btn.value = "Reset";
+        }, 1000);
+      }
     }
+    numberAppear();
   }
-  numberAppear();
 });
