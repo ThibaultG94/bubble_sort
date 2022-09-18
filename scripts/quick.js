1ef98cce0343b9ee9bf3b7d5c4ea765186faea69
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-18 22:46:31 +0200

    quicksort algo console.log

--- scripts/quick.js
+++ scripts/quick.js
@@ -1,5 +1,25 @@
 function quickSort(array) {
-  console.log(array);
-}
+  if (array.length === 1) {
+    return array;
+  }
 
-quickSort(numberQuick);
+  const pivot = array[array.length - 1];
+  let arrLeft = [];
+  let arrRight = [];
+
+  for (let i = 0; i < array.length - 1; i++) {
+    array[i] < pivot ? arrLeft.push(array[i]) : arrRight.push(array[i]);
+  }
+
+  if (arrLeft.length > 0 && arrRight.length > 0) {
+    console.log(arrLeft, pivot, arrRight);
+    return [...quickSort(arrLeft), pivot, ...quickSort(arrRight)];
+  } else if (arrLeft.length > 0) {
+    console.log(arrLeft);
+    return [...quickSort(arrLeft), pivot];
+  } else {
+    console.log(arrRight);
+    return [pivot, ...quickSort(arrRight)];
+  }
+}
+console.log(quickSort(numberQuick));
