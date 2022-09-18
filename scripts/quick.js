dba6d6c5f95bb99a0543ad0f78f6d87fcfcaff6c
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-09-18 23:44:16 +0200

    ...

--- scripts/quick.js
+++ scripts/quick.js
@@ -1,25 +1,66 @@
-function quickSort(array) {
-  if (array.length === 1) {
-    return array;
-  }
+// function quickSort(array) {
+//   if (array.length === 1) {
+//     return array;
+//   }
 
-  const pivot = array[array.length - 1];
-  let arrLeft = [];
-  let arrRight = [];
+//   const pivot = array[array.length - 1];
+//   let arrLeft = [];
+//   let arrRight = [];
 
-  for (let i = 0; i < array.length - 1; i++) {
-    array[i] < pivot ? arrLeft.push(array[i]) : arrRight.push(array[i]);
-  }
+//   for (let i = 0; i < array.length - 1; i++) {
+//     array[i] < pivot ? arrLeft.push(array[i]) : arrRight.push(array[i]);
+//   }
 
-  if (arrLeft.length > 0 && arrRight.length > 0) {
-    console.log(arrLeft, pivot, arrRight);
-    return [...quickSort(arrLeft), pivot, ...quickSort(arrRight)];
-  } else if (arrLeft.length > 0) {
-    console.log(arrLeft);
-    return [...quickSort(arrLeft), pivot];
+//   if (arrLeft.length > 0 && arrRight.length > 0) {
+//     console.log(arrLeft, pivot, arrRight);
+//     return [...quickSort(arrLeft), pivot, ...quickSort(arrRight)];
+//   } else if (arrLeft.length > 0) {
+//     console.log(arrLeft);
+//     return [...quickSort(arrLeft), pivot];
+//   } else {
+//     console.log(arrRight);
+//     return [pivot, ...quickSort(arrRight)];
+//   }
+// }
+// console.log(quickSort(numberQuick));
+
+btn.addEventListener("click", () => {
+  let num4 = document.querySelectorAll("#sortQuick > td");
+  let n = 0;
+
+  if (btn.value == "Reset") {
+    window.location.reload();
   } else {
-    console.log(arrRight);
-    return [pivot, ...quickSort(arrRight)];
+    btn.style.transform = "translateY(500px)";
   }
-}
-console.log(quickSort(numberQuick));
+
+  function quickSort(array) {
+    setTimeout(() => {
+      if (array.length === 1) {
+        return array;
+      }
+
+      const pivot = array[array.length - 1];
+
+      for (let i = 0; i < num4.length; i++) {
+        if (num4[i].textContent == pivot) {
+          num4[i].style.background = "#a9a9ff";
+        }
+      }
+
+      let arrLeft = [];
+      let arrRight = [];
+
+      if (n < array.length - 1) {
+        if (array[n] < pivot) {
+          arrLeft.push(array[n]);
+          console.log(arrLeft);
+        } else {
+          arrRight.push(array[n]);
+          console.log(arrRight);
+        }
+      }
+    }, 500);
+  }
+  quickSort(numberQuick);
+});
